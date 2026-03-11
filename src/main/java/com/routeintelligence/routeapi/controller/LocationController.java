e336d95c8b3acacec1b1042ec29acf9df827e374
Iñaki Ramos <iramositurria@gmail.com>
2026-03-11 12:58:34 +0100

    improve location service and location controller

---
+++ src/main/java/com/routeintelligence/routeapi/controller/LocationController.java
@@ -0,0 +1,5 @@
+package com.routeintelligence.routeapi.controller;
+
+public class LocationController {
+
+}
--- src/main/java/com/routeintelligence/routeapi/service/LocationService.java
+++ src/main/java/com/routeintelligence/routeapi/service/LocationService.java
@@ -1,4 +1,20 @@
 package com.routeintelligence.routeapi.service;
 
+import com.routeintelligence.routeapi.repository.LocationRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service layer responsible for location-related business logic.
+ */
+@Service
 public class LocationService {
+
+    private final LocationRepository locationRepository;
+
+    /**
+     * Constructor injection of LocationRepository.
+     */
+    public LocationService(LocationRepository locationRepository){
+        this.locationRepository = locationRepository;
+    }
 }
